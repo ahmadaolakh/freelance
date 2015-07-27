5173b76d662dc12099ed2ea8892c223ff66bc7bf
Ahmad <dev.ahmed@sigmasqr.com>
2015-07-27 12:15:10 +0500

    temp change

---
+++ MvcApplication1/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class AuthController : Controller
+    {
+        //
+        // GET: /Auth/
+
+        public ActionResult signin()
+        {
+            return View();
+        }
+        public ActionResult home()
+        {
+            return View();
+        }
+
+    }
+}
